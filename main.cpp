f856073b40da4dbaf6c5037c502964df6d2e86dd
tamofplease <riddimdubstep@yahoo.co.jp>
2023-10-30 12:08:12 +0900

    🐛

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// "Copyright 2022 tam"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define Rep(i, n) for (int i = 1; i <= n; i++)
+#define ALL(v) v.begin(), v.end()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+
+#define ll int64_t
+
+using P = pair<int, int>;
+
+const int MOD = 1000000007;
+
+int main() { return 0; }
